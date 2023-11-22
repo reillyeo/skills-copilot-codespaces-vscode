f5a9a43bf64002176c0bce4c5ad20ad131fea39e
reillyeo <94123725+reillyeo@users.noreply.github.com>
2023-11-22 15:36:12 +0000

    Copilot third commit

---
+++ comments.js
@@ -0,0 +1,9 @@
+// Create web server application:
+// 1. Create a web server application that can listen to client requests.
+// 2. Create a web server application that can serve static files.
+// 3. Create a web server application that can serve dynamic files.
+// 4. Create a web server application that can handle different HTTP methods.
+// 5. Create a web server application that can handle different routes.
+// 6. Create a web server application that can handle different routes with different HTTP methods.
+// 7. Create a web server application that can handle different routes with different HTTP methods and serve static files.
+// 8. Create a web server application that can handle different routes with different HTTP methods and serve dynamic files.
